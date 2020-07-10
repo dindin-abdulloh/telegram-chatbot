46ff56942fa69a53ce42fbbe05ebd61235f1e834
Dindin Abduloh <50541680+adin17-alt@users.noreply.github.com>
2020-07-10 23:36:49 +0700

    second change

--- uinsgdb_api.py
+++ uinsgdb_api.py
@@ -1,15 +1,29 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
-def command_handling_fn(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id,text="Hallo selamat pagi")
+
+
+def start(update, context):
+    update.message.reply_text('Hi! Apa kamu sudah makan???')
+
+def message(update, context):
+    update.message.reply_text(update.message.text.upper())
+
+def command_handler(update, context):
+    update.message.reply_text(f"Command: {update.message.text}")
+
+def main():
+    """Start the bot."""
+    updater = Updater("889859284:AAFeKjOv3zDsclgPw09Q4X2lJOB6NvlVuso", use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start",start))
+    dp.add_handler(MessageHandler(Filters.text, message))
+    dp.add_handler(MessageHandler(Filters.command, command_handler))
+    updater.start_polling()
+    updater.idle()
+
+
+
 
 if __name__=="__main__":
-    updater = Updater(token="889859284:AAFeKjOv3zDsclgPw09Q4X2lJOB6NvlVuso", use_context=True)
-    dispatcher = updater.dispatcher
-
-    handler = CommandHandler("start",command_handling_fn)
-
-    dispatcher.add_handler(handler)
-
-    updater.start_polling()
+    main()
