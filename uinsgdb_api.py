b89bba0f91b7e84f7029e4bc909bbd46a4ff718b
Dindin Abduloh <50541680+adin17-alt@users.noreply.github.com>
2020-07-11 12:49:52 +0700

    add image and audio

--- uinsgdb_api.py
+++ uinsgdb_api.py
@@ -12,6 +12,23 @@ def message(update, context):
 def command_handler(update, context):
     update.message.reply_text(f"Command: {update.message.text}")
 
+def receive_image(update, context):
+    try:
+        print(update)
+        obj = context.bot.getFile(file_id=update.message.document.file_id)
+        obj.download()
+        update.message.reply_text("File has been download.")
+    except Exception as e:
+        print(str(e))
+
+def receive_audio(update, context):
+    try:
+        audio_obj = context.bot.getFile(file_id=update.message.audio.file_id)
+        audio_obj.download()
+        update.message.reply_text("Your audio has been processed")
+    except expression as e:
+        pass
+
 def main():
     """Start the bot."""
     updater = Updater("889859284:AAFeKjOv3zDsclgPw09Q4X2lJOB6NvlVuso", use_context=True)
